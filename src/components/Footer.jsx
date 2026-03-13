9e063efdc5ad6a83bbb34d29b3ed44a11e6e873e
Zahid Mohammed Bin Hydra <zahidbinhydra@gmail.com>
2026-03-13 13:31:08 -0700

    feat: initial storefront architecture, product navigation, and cart logic

---
+++ src/components/Footer.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div>
+    <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+        <div>
+            <img src={assets.logo} className='mb-5 w-32' alt="" />
+            <p className='w-full md:w-2/3 text-gray-600'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo tempora est eligendi perspiciatis, quos consectetur?
+            </p>
+        </div>
+
+        <div>
+          <p className='text-xl font-medium mb-5'>COMPANY</p>
+          <ul className='flex flex-col gap-1 text-gray-600'>
+              <li>Home</li>
+              <li>About</li>
+              <li>Delivery</li>
+              <li>Privacy Policy</li>
+           </ul>   
+        </div>
+
+        <div>
+          <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+          <ul className='flex flex-col gap-1 text-gray-600'>
+              <li>+91-799-342-3969</li>
+              <li>contact@foreveryou.com</li>
+          </ul>
+        </div>
+
+    </div>
+
+      <div>
+        <hr />
+        <p className='py-5 text-sm text-center'>copyright 2026@ fprever.com - All Rights Reserved.</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default Footer
