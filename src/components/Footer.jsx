1df109ac263ac9a5dbcfb5192cea75e1188a3686
Zahid Mohammed Bin Hydra <zahidbinhydra@gmail.com>
2026-04-01 07:11:24 -0700

    rebranded from Forever to Active Wear - updated logo, colors, brand name

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <div>
     <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
         <div>
-            <img src={assets.logo} className='mb-5 w-32' alt="" />
+            <img src={assets.logo} className='mb-5 w-40' alt="" />
             <p className='w-full md:w-2/3 text-gray-600'>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo tempora est eligendi perspiciatis, quos consectetur?
             </p>
@@ -26,7 +26,7 @@ const Footer = () => {
           <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
           <ul className='flex flex-col gap-1 text-gray-600'>
               <li>+91-799-342-3969</li>
-              <li>contact@foreveryou.com</li>
+              <li>contact@activewear.com</li>
           </ul>
         </div>
 
@@ -34,7 +34,7 @@ const Footer = () => {
 
       <div>
         <hr />
-        <p className='py-5 text-sm text-center'>copyright 2026@ fprever.com - All Rights Reserved.</p>
+        <p className='py-5 text-sm text-center'>© 2026 Active Wear. All Rights Reserved.</p>
       </div>
 
     </div>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -15,9 +15,9 @@ const Contact = () => {
           <img className='w-full md:max-w-100' src={assets.contact_img} alt="" />
           <div className='flex flex-col justify-center items-start gap-6'>
               <p className='font-semibold text-xl text-gray-600 '>Our Store</p>
-              <p className='text-gray-500'>510012 Zahid Bin Hydra <br />Suit 350, Washington, USA</p>
-              <p className='text-gray-500'>Tel: (412) 555-0123 <br /> Email: admin@forever.com</p>
-              <p className='font-semibold text-xl text-gray-600'>Carers at Forever</p>
+              <p className='text-gray-500'> 123 Performance Street <br />New York, NY 10001</p>
+              <p className='text-gray-500'>Tel: (412) 555-0123 <br /> Email: admin@activewear.com</p>
+              <p className='font-semibold text-xl text-gray-600'>Carers at Active Wear</p>
               <p className='text-gray-500'>Learn more about our teams and job openings.</p>
               <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
           </div>
