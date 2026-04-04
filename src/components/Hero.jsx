d24f8a554555c7ec504774f3dca112a8558a8bb7
Zahid Mohammed Bin Hydra <zahidbinhydra@gmail.com>
2026-04-04 08:33:45 -0700

    updated hero section with activewear image and leftside with updated text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { assets } from '../assets/assets'
 
 const Hero = () => {
-  return (
-    <div className='flex flex-col sm:flex-row border border-gray-400'>
+   return (
+    <div className='flex flex-col sm:flex-row border border-gray-400 bg-gray-50'>
         {/* Hero Left Side */}
-        <div className='w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0'>
+        <div className='w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0 '>
             <div className='text-[#414141]'>
                 <div className='flex items-center gap-2'>
                     <p className='w-8 md:w-11 h-0.5 bg-[#414141]'></p>
-                    <p className='font-medium text-sm md:text-base'>OUR BESTSELLERS</p>
+                    <p className='font-medium text-sm md:text-base'>ELITE TRAINING GEAR</p>
                 </div>
-                <h1 className='prata-regular text-3xl sm:py-3 lg:text-5xl leading-relaxed'>Latest Arrivals</h1>
+                <h1 className='prata-regular text-3xl sm:py-3 lg:text-5xl leading-relaxed'>Latest Drop </h1>
                 <div className='flex items-center gap-2'>
                     <p className='font-semibold text-sm md:text-base'>SHOP NOW</p>
                     <p className='w-8 md:w-11 h-0.5 bg-[#414141]'></p>
@@ -19,9 +19,10 @@ const Hero = () => {
             </div>
         </div>
         {/* Hero Right Side */}
-        <img src={assets.hero_img} className='w-full sm:w-1/2' alt="" />
+        <img src={assets.hero_img} className='w-full sm:w-1/2 object-cover' alt="" />
     </div>
-  )
+)
+       
 }
 
 export default Hero
