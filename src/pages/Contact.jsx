b9835f0dd8ea339850e45f7bd5998107b4e1387a
Zahid Mohammed Bin Hydra <zahidbinhydra@gmail.com>
2026-04-29 04:01:38 -0700

    updated contact and about images with athletic lifestyle photos

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
       </div>
 
       <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28'>
-          <img className='w-full md:max-w-100' src={assets.contact_img} alt="" />
+          <img className='w-full md:max-w-120 object-cover' src={assets.contact_img} alt="" />
           <div className='flex flex-col justify-center items-start gap-6'>
               <p className='font-semibold text-xl text-gray-600 '>Our Store</p>
               <p className='text-gray-500'> 123 Performance Street <br />New York, NY 10001</p>
